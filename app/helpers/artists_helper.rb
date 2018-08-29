e990670b67943d5b23e1dd7625287b53283986c8
derrick b <dbozich1@gmail.com>
2018-08-29 21:49:34 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,7 +4,11 @@ module ArtistsHelper
   end
 
   def artist_select(artist, song)
-
+    if post.artist.nil?
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
   end
 
 
